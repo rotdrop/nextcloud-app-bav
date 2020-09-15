c61239eb9286901e81665d875c7379eea9099fd2
Claus-Justus Heine <himself@claus-justus-heine.de>
2020-09-15 19:04:51 +0200

    Configure bav backend.

--- appinfo/app.php
+++ appinfo/app.php
@@ -12,3 +12,21 @@
 if ((@include_once __DIR__ . '/../vendor/autoload.php')===false) {
 	throw new Exception('Cannot include autoload. Did you run install dependencies using composer?');
 }
+
+$ncConfig = \OC::$server->getSystemConfig();
+$bavConfig = new \malkusch\bav\DefaultConfiguration();
+
+$dbType = $ncConfig->getValue('dbtype', 'mysql');
+$dbHost = $ncConfig->getValue('dbhost', 'localhost');
+$dbName = $ncConfig->getValue('dbname', false);
+$dbUser = $ncConfig->getValue('dbuser', false);
+$dbPass = $ncConfig->getValue('dbpassword', false);
+
+$dbURI = $dbType.':'.'host='.$dbHost.';dbname='.$dbName;
+
+$pdo = new \PDO($dbURI, $dbUser, $dbPass);
+$bavConfig->setDataBackendContainer(new \malkusch\bav\PDODataBackendContainer($pdo));
+
+$bavConfig->setUpdatePlan(new \malkusch\bav\AutomaticUpdatePlan());
+
+\malkusch\bav\ConfigurationRegistry::setConfiguration($bavConfig );
